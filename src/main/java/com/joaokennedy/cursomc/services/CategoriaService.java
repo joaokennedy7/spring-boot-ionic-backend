66de5851eaa2c945621086b18b455938c3552f3a
João Kennedy <joao.kennedy707@gmail.com>
2020-11-30 17:09:49 -0300

    Listando todas categorias com DTO

--- src/main/java/com/joaokennedy/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaokennedy/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.joaokennedy.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.hibernate.ObjectNotFoundException;
@@ -42,4 +43,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
 	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	
 }
