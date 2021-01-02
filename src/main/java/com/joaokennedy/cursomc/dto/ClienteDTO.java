a6becee0c8542e0d50941b3eafff3058dc9b7602
João Kennedy <joao.kennedy707@gmail.com>
2021-01-02 15:07:15 -0300

    PUT, DELETE, GET para Cliente

--- src/main/java/com/joaokennedy/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/joaokennedy/cursomc/dto/ClienteDTO.java
@@ -2,31 +2,34 @@ package com.joaokennedy.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-
 import org.hibernate.validator.constraints.Length;
+import com.joaokennedy.cursomc.domain.Cliente;
 
-import com.joaokennedy.cursomc.domain.Categoria;
-import javax.validation.constraints.NotEmpty;
-
-public class CategoriaDTO implements Serializable {
+public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=120, message="0 tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 	
-	public CategoriaDTO() {
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
 		
 	}
-	
-	public CategoriaDTO(Categoria obj) {
+
+	public ClienteDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();
+		email = obj.getEmail();
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
@@ -41,6 +44,14 @@ public class CategoriaDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	
